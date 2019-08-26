fefe0c506ad30cf691c9e9794a6b3b1ac3d7da0e
Eyrenna <claudia365x@gmail.com>
2019-08-26 21:13:06 +0200

    Creada clase TarjetaPrepago con atributos, constructores y getters/setters

---
+++ src/main/java/encinas/TarjetaPrepago.java
@@ -0,0 +1,64 @@
+package encinas;
+
+public class TarjetaPrepago {
+
+    //PROPIEDADES
+    private String numeroTelefono = null;
+    private DNI dni = null;
+    private Double saldo = 0d;
+    private Hora consumo = null;
+
+
+    //CONSTRUCTOR
+
+    public TarjetaPrepago() {
+    }
+
+    public TarjetaPrepago(String numeroTelefono, DNI dni, Double saldo) {
+        this.numeroTelefono = numeroTelefono;
+        this.dni = dni;
+        this.saldo = saldo;
+    }
+
+
+    //MÉTODO
+    //Getters & Setters
+
+    public String getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public void setNumeroTelefono(String numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    public DNI getDni() {
+        return dni;
+    }
+
+    public void setDni(DNI dni) {
+        this.dni = dni;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Hora getConsumo() {
+        return consumo;
+    }
+
+    public void setConsumo(Hora consumo) {
+        this.consumo = consumo;
+    }
+
+
+    //MÉTODOS
+    //Otros
+
+
+}
